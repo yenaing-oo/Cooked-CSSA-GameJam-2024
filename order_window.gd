5518ca75de63c6d0ed6990fd818d084aa77e36f9
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2024-11-16 11:27:41 -0600

    make timer label work
    put a count down that restarts when it hits 0

---
+++ order_window.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+const TAKE_ORDER_TIMER = 11
+
+var time = TAKE_ORDER_TIMER
+
+@onready var timer: Label3D = $Timer/timer
+
+
+
+func _ready() -> void:
+	$Timer.start()
+	
+	
+
+
+
+func _on_timer_timeout() -> void:
+	time = time - 1
+	timer.text = str(time)
+	if(time == 0):
+		time = TAKE_ORDER_TIMER
