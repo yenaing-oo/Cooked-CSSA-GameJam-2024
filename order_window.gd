7f9691bff48b6523f6b169f6dccbeb3c48a3435c
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2024-11-16 12:44:12 -0600

    Add penalty for not clicking the button
    -1 rating for not taking the order

--- order_window.gd
+++ order_window.gd
@@ -3,13 +3,16 @@ extends Node3D
 #countdown timer for taking orders.
 const TAKE_ORDER_TIMER = 11
 
+var currRating = 5
 
 var time = TAKE_ORDER_TIMER
 
 @onready var timer: Label3D = $Timer/timer
 
+
 func _ready() -> void:
 	$Timer.start()
+	print("Rating: " + str(currRating))
 
 
 
@@ -17,4 +20,6 @@ func _on_timer_timeout() -> void:
 	time = time - 1
 	timer.text = str(time)
 	if(time == 0):
+		currRating = currRating-1
+		print("Rating: " + str(currRating))
 		time = TAKE_ORDER_TIMER
