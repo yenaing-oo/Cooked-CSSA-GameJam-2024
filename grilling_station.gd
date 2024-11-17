fc79ef82e6e127adc132ce2dbb19f319b5a51e79
AmritpalBedi <125513113+AmritpalBedi@users.noreply.github.com>
2024-11-17 00:25:26 -0600

    Add progress bars

--- grilling_station.gd
+++ grilling_station.gd
@@ -9,17 +9,20 @@ var burning = false
 var cooking_start_time
 var burning_start_time
 var loading_bar
-var loading_bar_front
-var loading_bar_front_offset
+var loading_bar_cooking
+var loading_bar_cooking_offset
+var loading_bar_burnt
+var loading_bar_burnt_offset
 
 @onready var player = get_parent().get_node("player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	loading_bar = $LoadingBar
-	loading_bar_front = $LoadingBar/LoadingBarFront
-	loading_bar_front_offset = loading_bar_front.position.x
-	
+	loading_bar_cooking = $LoadingBar/LoadingBarCooking
+	loading_bar_cooking_offset = loading_bar_cooking.position.x
+	loading_bar_burnt = $LoadingBar/LoadingBarBurnt
+	loading_bar_burnt_offset = loading_bar_burnt.position.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -47,18 +50,26 @@ func update_cooking_timer():
 		burning = true
 		
 	else:
-		loading_bar_front.scale = Vector3(time_fraction, 1, 1)
-		loading_bar_front.position = Vector3((1-time_fraction) * loading_bar_front_offset, 0, 0)
+		loading_bar_burnt.visible = false
+		loading_bar_cooking.visible = true
+		loading_bar_cooking.scale = Vector3(time_fraction, 1, 1)
+		loading_bar_cooking.position = Vector3((1-time_fraction) * loading_bar_cooking_offset, 0, 0)
 
 #Just have the food burn after the timer goes off
 func update_burning_timer():
 	var time_elapsed = Time.get_unix_time_from_system() - burning_start_time
-	
+	var time_fraction = time_elapsed / BURNING_TIME
+
 	if time_elapsed >= BURNING_TIME:
 		print("Food Burnt!")
 		burning = false
 		cooked = false
 		reset_loading_bar()
+	else:
+		loading_bar_cooking.visible = false
+		loading_bar_burnt.visible = true
+		loading_bar_burnt.scale = Vector3(time_fraction, 1, 1)
+		loading_bar_burnt.position = Vector3((1-time_fraction) * loading_bar_burnt_offset, 0, 0)
 
 #Used by the player to reset the station, only returns true if the food was cooked (not burnt)
 func grab_cooked_food() -> bool:
@@ -70,6 +81,13 @@ func grab_cooked_food() -> bool:
 	return output
 
 func reset_loading_bar():
+	cooking = false
+	burning = false
 	loading_bar.visible = 0
-	loading_bar_front.scale = Vector3(0.001, 1, 1)
-	loading_bar_front.position = Vector3(loading_bar_front_offset, 0, 0)
+	loading_bar_cooking.visible = false
+	loading_bar_cooking.scale = Vector3(0.001, 1, 1)
+	loading_bar_cooking.position = Vector3(loading_bar_cooking_offset, 0, 0)
+
+	loading_bar_burnt.visible = false
+	loading_bar_burnt.scale = Vector3(0.001, 1, 1)
+	loading_bar_burnt.position = Vector3(loading_bar_burnt_offset, 0, 0)
